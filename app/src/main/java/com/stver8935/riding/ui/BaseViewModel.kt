8c724467ee1a98ffb95d7f5072f48906ccd2fc6b
stver8935 <gksrlgud10@naver.com>
2024-10-07 20:52:59 +0900

    refactor : 불필요 코드 제거

--- app/src/main/java/com/stver8935/riding/ui/BaseViewModel.kt
+++ app/src/main/java/com/stver8935/riding/ui/BaseViewModel.kt
@@ -1,9 +1,6 @@
 package com.stver8935.riding.ui
 
 import androidx.lifecycle.ViewModel
-import com.stver8935.riding.domain.network.NetworkDetector
-import com.stver8935.riding.domain.network.NetworkStateListener
-
 /**
  *
  *
@@ -12,17 +9,5 @@ import com.stver8935.riding.domain.network.NetworkStateListener
  * @author     : hangihyeong
  * @since      : 2024. 10. 1.
  */
-abstract class BaseViewModel:  ViewModel(),NetworkStateListener{
-    private lateinit var networkDetector: NetworkDetector
-    init {
-        networkDetector.registerCallback()
-    }
-
-    override fun onAvailableNetwork() {
-        TODO(" do work .. ")
-    }
-
-    override fun onLostNetwork() {
-        TODO(" do work.. ")
-    }
+abstract class BaseViewModel:  ViewModel(){
 }
